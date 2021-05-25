bee2d6d646af308e492f372fa1d0bec45efa6438
khongchai <world1955@hotmail.com>
2021-05-25 14:52:29 +0700

    Updates

---
+++ component---src-pages-404-js-373a6a542061c2dba7e3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("gRST"),o=n("ElpU");t.default=function(){return l.a.createElement(u.default,null,l.a.createElement(o.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-373a6a542061c2dba7e3.js.map
