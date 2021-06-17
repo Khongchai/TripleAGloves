087277d585a4111217e558ad57d3bdaf49ca707e
khongchai <world1955@hotmail.com>
2021-06-17 19:13:37 +0700

    Updates

--- component---src-pages-gloves-tsx-b760f9528472f1ed9352.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{mtcc:function(e,n,t){"use strict";t.r(n);var c=t("hGi/"),a=t("q1tI"),i=t.n(a);n.default=function(e){return Object(c.a)(e),i.a.createElement("div",null,"Gloves page")}}}]);
-//# sourceMappingURL=component---src-pages-gloves-tsx-b760f9528472f1ed9352.js.map
